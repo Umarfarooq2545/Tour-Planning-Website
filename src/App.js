a7df0ea4a1ea4129806f9f3408477699299e09b4
Umarfarooq2545 <nowumar2545@gmail.com>
2022-11-28 09:22:13 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Your Code Here</h1>
+    </div>
+  );
+}
